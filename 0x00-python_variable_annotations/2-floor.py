ebb093618d1610cf5eac8d83dbfa235029fae940
George-byt <jmoralesvalencia750@gmail.com>
2022-01-31 16:46:07 -0500

    Basic annotations-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Python file that contain  a type-annotated function <floor>
+which takes a float <n> as argument and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Type-annotated function <floor> that takes
+    as input a real number x, and gives as output
+    the greatest integer less than or equal to a given number.
+    """
+    return int(n)
