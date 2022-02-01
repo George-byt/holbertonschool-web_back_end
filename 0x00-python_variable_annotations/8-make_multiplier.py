dd10d3bb4f388a28baff2ddaa2ae6f15f5937ad6
George-byt <jmoralesvalencia750@gmail.com>
2022-01-31 21:25:28 -0500

    Complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Python file that contains a type-annotated function <make_multiplier> """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    type-annotated function <make_multiplier>
+    that takes a float <multiplier> as argument
+    and returns a function that multiplies a float by
+    multiplier
+    """
+    def fn(n: float):
+        return n * multiplier
+    return fn
