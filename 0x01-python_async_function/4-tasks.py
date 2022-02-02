d24b205f1da72c952251c893cb81163cdf928ac1
George-byt <jmoralesvalencia750@gmail.com>
2022-02-01 23:32:06 -0500

    Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-""" Python file that contains an asynchronous coroutine """
+""" Tasks """
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    <<task_wait_n>> function The code is nearly identical
-    to wait_n except task_wait_random is being called.
+    """async routine
+    Args:
+        n (int): Number of times
+        max_delay (int): Waiting top number
+    Returns:
+        List[float]: list of all the delays
     """
     delay_list = [task_wait_random(max_delay) for _ in range(n)]
     return [await delay_list
