880688e7b49870680802616d20be6538e74e12d3
George-byt <jmoralesvalencia750@gmail.com>
2022-02-01 23:17:45 -0500

    Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,6 +8,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     << task_wait_n >> function
+    The code is nearly identical
+    to wait_n except task_wait_random
+    is being called.
     """
     delay_list = [task_wait_random(max_delay) for _ in range(n)]
     return [await delay_list
