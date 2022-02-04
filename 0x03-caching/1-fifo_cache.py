a1ea6fd5342994bc67328a6fc30ad66e6372cbc6
George-byt <jmoralesvalencia750@gmail.com>
2022-02-04 18:51:18 -0500

    FIFO caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Python file that contains a <FIFOCache> class"""
+from typing import Union
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    <FIFOCache> is a caching system
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key: str, item: str) -> None:
+        """Add value en cache."""
+        if key and item:
+            new_dict = {key: item}
+            self.isFillCache()
+            new_dict.update(self.cache_data)
+            self.cache_data = new_dict
+
+    def get(self, key: str) -> Union[None, object]:
+        """Get value of cache"""
+        if key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
+
+    def isFillCache(self) -> None:
+        """check if cache not is fill"""
+        if len(self.cache_data.keys()) >= self.MAX_ITEMS:
+            delete_item = self.cache_data.popitem()[0]
+            print(f'DISCARD: {delete_item}')
+    
