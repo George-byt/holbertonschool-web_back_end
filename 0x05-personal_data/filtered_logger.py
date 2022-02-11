7b4d9c53435db6a31210669c3238557bbbeb1382
George-byt <jmoralesvalencia750@gmail.com>
2022-02-11 13:39:52 -0500

    Task #0 => Regex-ing

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,13 +4,15 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Arguments:
     => fields: A list of strings representing all fields to abfuscate
     => redaction: A string representing by what the field will be obfuscated
     => message: A string representing the log line
-    => separator: A string representing by which character is separating all fields in the log line (message)
+    => separator: A string representing by which character is separating all
+    fields in the log line (message)
     """
     for field in fields:
         message = re.sub(f"{field}=.*?{separator}",
