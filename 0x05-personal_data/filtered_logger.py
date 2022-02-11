a7851d2ed766644b222bd7e531e50a7c5ee319ac
George-byt <jmoralesvalencia750@gmail.com>
2022-02-11 14:16:33 -0500

    Log formatter

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,34 @@
 """ Python file about 0x05. Personal data project """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format method to class
+
+        Arguments:
+        => record: (logging.LogRecord)
+
+        Returns:
+        => str
+        """
+        msg = filter_datum(self.fields, self.REDACTION,
+                           super().format(record), self.SEPARATOR)
+        return msg
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
