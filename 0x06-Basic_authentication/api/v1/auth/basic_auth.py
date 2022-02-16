53a02ba1db3d8db7565306f559bf6c56944f55f8
George-byt <jmoralesvalencia750@gmail.com>
2022-02-15 23:25:28 -0500

    Basic - Base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,7 +25,8 @@ class BasicAuth(Auth):
                 else header[1])
 
     def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str) -> str:
+                                           base64_authorization_header: str
+                                           ) -> str:
         """
         returns the decoded value of a Base64
         string base64_authorization_header
