9cba3fa5e458423bf29782508bf5b40ded00aa46
George-byt <jmoralesvalencia750@gmail.com>
2022-02-15 20:03:55 -0500

    Basic - Base64 part

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 """ Python file that contains a class to manage the API authentication """
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        """
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+        header = authorization_header.split(' ')
+        return (None if not bool(re.search('^Basic ', authorization_header))
+                else header[1])
