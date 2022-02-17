cd74d1e8c4bc1577bc5728b2a18053f17fa8ab9f
George-byt <jmoralesvalencia750@gmail.com>
2022-02-17 14:00:45 -0500

    Create a session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Python file that contains the SessionAuth class """
 from api.v1.auth.auth import Auth
+import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -9,4 +11,15 @@ class SessionAuth(Auth):
     Args:
         Auth ([type]): Inherits from Auth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Method that creates a Session ID
+        for a user_id
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
