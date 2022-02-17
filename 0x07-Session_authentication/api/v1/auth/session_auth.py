ea04b0027c4b501617a3bca8d3e94469d6dea134
George-byt <jmoralesvalencia750@gmail.com>
2022-02-17 13:13:25 -0500

    Empty session

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python file that contains the SessionAuth class """
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SeccionAuth class that inherits from Auth.
+    """
+    pass
