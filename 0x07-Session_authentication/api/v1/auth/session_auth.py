517fb872ff044c3f8b9311e9fa776f8b1a705339
George-byt <jmoralesvalencia750@gmail.com>
2022-02-18 13:55:04 -0500

    Create a session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,7 @@
 ##!/usr/bin/env python3
 """ Python file that contains the SessionAuth class """
 from api.v1.auth.auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """
@@ -10,4 +10,14 @@ class SessionAuth(Auth):
         any overloading
         validate the "switch" by using environment variables
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        create_session method that creates a Session ID for user_id
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
