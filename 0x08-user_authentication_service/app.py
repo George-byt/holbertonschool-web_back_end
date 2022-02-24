998ec5841d4bbaf4c8f21690a4bad275a549c155
George-byt <jmoralesvalencia750@gmail.com>
2022-02-24 10:47:55 -0500

    Register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """ Basic Flask app """
 from flask import Flask
+from flask.globals import request
 from flask.json import jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
-
+ 
 @app.route('/')
 def welcome():
     """
@@ -13,6 +16,18 @@ def welcome():
     """
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'])
+def users():
+    """
+    /users POST end-point
+    """
+    email = request.form['email']
+    passw = request.form['password']
+    try:
+        user = AUTH.register_user(email, passw)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
