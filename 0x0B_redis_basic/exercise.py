96498b19c4421fffaf1cc2f3c8a2f234fc5cedde
George-byt <jmoralesvalencia750@gmail.com>
2022-03-15 16:36:15 -0500

    Retrieving lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
 Writing strings to Redis, Reading from Redis and recovering original type,
-Incrementing values, Storing lists, Retrieving lists 
+Incrementing values, Storing lists, Retrieving lists
 """
-from typing import Union, Callable, Optional
+from typing import Union, Callable, Optional, Any
 import redis
 import uuid
 from functools import wraps
