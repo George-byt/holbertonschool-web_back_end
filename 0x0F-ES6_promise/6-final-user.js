8405074926c37f61ffd6c535739c9744ba8880bc
George-byt <jmoralesvalencia750@gmail.com>
2022-03-29 22:07:53 -0500

    Handle multiple promises

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,5 +1,6 @@
 import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject'
+import uploadPhoto from './5-photo-reject';
+
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const stats = [];
   await signUpUser(firstName, lastName)
