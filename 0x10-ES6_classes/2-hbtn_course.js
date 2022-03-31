3d17fc2648517616d03556b4ca7bc62ab12c5b26
George-byt <jmoralesvalencia750@gmail.com>
2022-03-31 14:58:29 -0500

    A Course, Getters, and Setters

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) !== 'string') throw TypeError('Name must be a string')
+    if (typeof (length) !== 'number') throw TypeError('Length must be a number')
+    students.forEach((students) => {
+      if (typeof (students) !== 'string') throw TypeError('Students must be an array of strings')
+    });
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getters
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setters
+
+  set name(name) {
+    if (typeof (name) !== 'string') throw TypeError('Name must be a string')
+    return this._name = name;
+  }
+
+  set length(length) {
+    if (typeof (length) !== 'number') throw TypeError('Length must be a number')
+    this._length = length;
+  }
+
+  set students(students) {
+    students.forEach((students) => {
+      if (typeof (students) !== 'string') throw TypeError('Students must be an array of strings')
+    });
+    this._students = students;
+  }
+}
