09f1377d0a97f57c2806785b7d0285ed46ea5e43
George-byt <jmoralesvalencia750@gmail.com>
2022-03-31 15:10:05 -0500

    A Building

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,19 +1,15 @@
 export default class Building {
-  constructor (sqft) {
+  constructor(sqft) {
     if (this.constructor !== Building) {
       if (!this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
-
   // Get
-
   get sqft() {
     return this._sqft;
   }
-
   // Set
-
   set sqft(sqft) {
     return this._sqft = sqft;
   }
