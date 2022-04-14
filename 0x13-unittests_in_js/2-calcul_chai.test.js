5a2dab103ceec87333496a26ea8373f3873543b1
George-byt <jmoralesvalencia750@gmail.com>
2022-04-14 16:22:53 -0500

    Basic test using Chai assertion library

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,6 +1,6 @@
 'use strict';
 const chai = require('chai');
-const calculateNumber = require('./1-calcul.js');
+const calculateNumber = require('./2-calcul_chai.js');
 
 describe('calculateNumber type == SUM', () => {
   it('checks the output', () => {
