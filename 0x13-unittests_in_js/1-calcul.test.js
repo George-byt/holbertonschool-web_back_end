716fca9c1dd77c10ccceb1f5ceaafba3fab70a7b
George-byt <jmoralesvalencia750@gmail.com>
2022-04-14 12:43:09 -0500

    Combining descriptions

---
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,26 @@
+'use strict';
+const assert = require('assert');
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber type == SUM', () => {
+    it('checks the output', () => {
+        assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+        assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+        assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
+        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        assert.strictEqual(calculateNumber('SUM', 4.5, 1.4), 6);
+        assert.strictEqual(calculateNumber('SUM', 0.0, 0), 0);
+        assert.strictEqual(calculateNumber('SUM', -1, 1), 0);
+        assert.strictEqual(calculateNumber('SUM', 1, -1), 0);
+        assert.strictEqual(calculateNumber('SUM', -1, -1), -2);
+    });
+});
+
+describe('calculateNumber type == DIVIDE', () => {
+    it('check the output', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 2, 2.5), 0.6666666666666666);
+      assert.strictEqual(calculateNumber('DIVIDE', 0.0, 2), 0);
+      assert.strictEqual(calculateNumber('DIVIDE', -1, 1), -1);
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+    });
+});
