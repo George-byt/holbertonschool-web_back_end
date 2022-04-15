8a506bea2a3e02245a65c15d96e71c0047dbc74c
George-byt <jmoralesvalencia750@gmail.com>
2022-04-14 19:24:39 -0500

    Stubs

---
+++ 0x13-unittests_in_js/4-payment.js
@@ -0,0 +1,8 @@
+'use strict'
+const Utils = require('./utils.js');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${res}`);
+}
+module.exports = sendPaymentRequestToApi;
